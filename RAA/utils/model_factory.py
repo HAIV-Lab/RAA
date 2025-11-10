e601b15e352a168bb67a2cb8397f626ef09fa961
chessplayer9 <97439180+chessplayer9@users.noreply.github.com>
2025-11-10 16:56:11 +0800

    Delete RAA directory

--- RAA/utils/model_factory.py
+++
@@ -1,7 +0,0 @@
-def get_model(model_name, args):
-    name = model_name.lower()
-    if name=="adapter":
-        from models.RAA import Learner
-        return Learner(args)
-    else:
-        assert 0
